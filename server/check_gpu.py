32b521493fbaec224107b096a0e240e16a1c8dd1
Justin Huang <yinting_justin_huang@outlook.com>
2024-08-29 13:51:02 +0100

    modify data prep

--- server/check_gpu.py
+++ server/check_gpu.py
@@ -1,9 +1,10 @@
 import torch
 
+print(torch.__version__)  # PyTorch version
+print(torch.version.cuda)  # CUDA version
+
 print(torch.cuda.is_available())  # Should return True if GPU is available
 print(torch.cuda.device_count())  # Should return the number of GPUs
 print(torch.cuda.current_device())  # Should return the index of the current GPU
 print(torch.cuda.get_device_name(0))  # Should return the name of the GPU
 
-print(torch.__version__)  # PyTorch version
-print(torch.version.cuda)  # CUDA version
