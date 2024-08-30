f4e1d61795037e4097ff686842fceeb0cafcd81a
Justin Huang <yinting_justin_huang@outlook.com>
2024-08-30 13:33:15 +0100

    add gpu support

--- server/opus-mt-en-de.py
+++ server/opus-mt-en-de.py
@@ -32,12 +32,12 @@ source_texts = data['source_texts']
 target_texts = data['target_texts']
 
 # Compute BLEU score
-baseline_bleu = compute_bleu_score(device,model, tokenizer, source_texts, target_texts)
-print(baseline_bleu) 
+# baseline_bleu = compute_bleu_score(device,model, tokenizer, source_texts, target_texts)
+# print(baseline_bleu) 
 
-baseline_fscore = compute_character_fscore(device,model, tokenizer, source_texts, target_texts)
+# baseline_fscore = compute_character_fscore(device,model, tokenizer, source_texts, target_texts)
 
-print(baseline_fscore) 
+# print(baseline_fscore) 
 
 original_atten_block_weight_array_encoder_0 = extract_weight_array(model.model.encoder.layers[0])
 original_atten_block_weight_array_encoder_1 = extract_weight_array(model.model.encoder.layers[1])
