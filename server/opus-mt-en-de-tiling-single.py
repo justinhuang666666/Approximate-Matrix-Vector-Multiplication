49f8ecf0cdd33a4203045261ada760db36540180
Justin Huang <yinting_justin_huang@outlook.com>
2024-08-30 16:50:35 +0100

    add gpu support

---
+++ server/opus-mt-en-de-tiling-single.py
@@ -0,0 +1,105 @@
+import torch
+from transformers import MarianMTModel, MarianTokenizer
+import json
+from utils import *
+import os
+import sys
+
+import warnings
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+# Add utility directories dynamically
+sys.path.append(os.path.join(parent_dir, 'iterative_approximation'))
+from iterative_approximation_gpu import *
+
+# Load the tokenizer and model
+model_name = "Helsinki-NLP/opus-mt-en-de"
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+model.eval()
+
+# Check if GPU is available and move model to GPU if possible
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+model.to(device)
+
+# Load the JSON file
+with open('translations.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Extract the source and target texts
+source_texts = data['source_texts']
+target_texts = data['target_texts']
+
+# Compute BLEU score
+baseline_bleu = compute_bleu_score(device,model, tokenizer, source_texts, target_texts)
+print("Baseline BLEU Score")
+print(baseline_bleu) 
+
+baseline_fscore = compute_character_fscore(device,model, tokenizer, source_texts, target_texts)
+print("Baseline Character Fscore")
+print(baseline_fscore) 
+
+# original_atten_block_weight_array_encoder_0 = extract_weight_array(model.model.encoder.layers[0])
+# original_atten_block_weight_array_encoder_1 = extract_weight_array(model.model.encoder.layers[1])
+# original_atten_block_weight_array_encoder_2 = extract_weight_array(model.model.encoder.layers[2])
+# original_atten_block_weight_array_encoder_3 = extract_weight_array(model.model.encoder.layers[3])
+# original_atten_block_weight_array_encoder_4 = extract_weight_array(model.model.encoder.layers[4])
+# original_atten_block_weight_array_encoder_5 = extract_weight_array(model.model.encoder.layers[5])
+
+# W0 = WeightArray(original_atten_block_weight_array_encoder_0,'array',0.001,1,1,512,512)
+# W1 = WeightArray(original_atten_block_weight_array_encoder_1,'array',0.001,1,1,512,512)
+# W2 = WeightArray(original_atten_block_weight_array_encoder_2,'array',0.001,1,1,512,512)
+# W3 = WeightArray(original_atten_block_weight_array_encoder_3,'array',0.001,1,1,512,512)
+# W4 = WeightArray(original_atten_block_weight_array_encoder_4,'array',0.001,1,1,512,512)
+# W5 = WeightArray(original_atten_block_weight_array_encoder_5,'array',0.001,1,1,512,512)
+
+
+# mse = []
+# bleu_scores = []
+# compression_ratio = []
+
+# for i in range(150):  #150
+#         atten_block_weight_array_0 = W0.iterative_approximation(1)
+#         atten_block_weight_array_1 = W1.iterative_approximation(1)
+#         atten_block_weight_array_2 = W2.iterative_approximation(1)
+#         atten_block_weight_array_3 = W3.iterative_approximation(1)
+#         atten_block_weight_array_4 = W4.iterative_approximation(1)
+#         atten_block_weight_array_5 = W5.iterative_approximation(1)
+
+# from tqdm import tqdm
+
+# with tqdm(total=18, desc='Processing', unit='iteration') as pbar:
+#     for i in range(18): #18
+#         for i in range(5):  #5
+#             atten_block_weight_array_0 = W0.iterative_approximation(1)
+#             atten_block_weight_array_1 = W1.iterative_approximation(1)
+#             atten_block_weight_array_2 = W2.iterative_approximation(1)
+#             atten_block_weight_array_3 = W3.iterative_approximation(1)
+#             atten_block_weight_array_4 = W4.iterative_approximation(1)
+#             atten_block_weight_array_5 = W5.iterative_approximation(1)
+
+#         W0_MSE = (W0.average_mse_array()+W1.average_mse_array()+W2.average_mse_array()+W3.average_mse_array()+W4.average_mse_array()+W5.average_mse_array())/6
+#         mse.append(W0_MSE)
+#         compression_ratio.append(W0.compression_ratio())
+
+#         set_layer_weight(model.model.encoder.layers[0],atten_block_weight_array_0)
+#         set_layer_weight(model.model.encoder.layers[1],atten_block_weight_array_1)
+#         set_layer_weight(model.model.encoder.layers[2],atten_block_weight_array_2)
+#         set_layer_weight(model.model.encoder.layers[3],atten_block_weight_array_3)
+#         set_layer_weight(model.model.encoder.layers[4],atten_block_weight_array_4)
+#         set_layer_weight(model.model.encoder.layers[5],atten_block_weight_array_5)
+
+#         bleu = compute_bleu_score(device, model, tokenizer,source_texts,target_texts)
+#         bleu_scores.append(bleu)
+
+#         pbar.update(1)
+
+
+# print(bleu_scores)
+
--- server/opus-mt-en-de.py
+++ server/opus-mt-en-de.py
@@ -4,6 +4,7 @@ import json
 from utils import *
 import os
 import sys
+import pandas as pd
 
 import warnings
 
@@ -37,69 +38,27 @@ source_texts = data['source_texts']
 target_texts = data['target_texts']
 
 # Compute BLEU score
-# baseline_bleu = compute_bleu_score(device,model, tokenizer, source_texts, target_texts)
-# print("Baseline BLEU Score")
-# print(baseline_bleu) 
+baseline_bleu = compute_bleu_score(device,model, tokenizer, source_texts, target_texts)
+print("Baseline BLEU Score")
+print(baseline_bleu) 
 
 baseline_fscore = compute_character_fscore(device,model, tokenizer, source_texts, target_texts)
 print("Baseline Character Fscore")
 print(baseline_fscore) 
 
-# original_atten_block_weight_array_encoder_0 = extract_weight_array(model.model.encoder.layers[0])
-# original_atten_block_weight_array_encoder_1 = extract_weight_array(model.model.encoder.layers[1])
-# original_atten_block_weight_array_encoder_2 = extract_weight_array(model.model.encoder.layers[2])
-# original_atten_block_weight_array_encoder_3 = extract_weight_array(model.model.encoder.layers[3])
-# original_atten_block_weight_array_encoder_4 = extract_weight_array(model.model.encoder.layers[4])
-# original_atten_block_weight_array_encoder_5 = extract_weight_array(model.model.encoder.layers[5])
+# Create a DataFrame to store the results in a reformatted style
+results = {
+    'Model': ['opus-mt-en-de'],
+    'BLEU Score': [baseline_bleu],
+    'Character Fscore': [baseline_fscore]
+}
 
-# W0 = WeightArray(original_atten_block_weight_array_encoder_0,'array',0.001,1,1,512,512)
-# W1 = WeightArray(original_atten_block_weight_array_encoder_1,'array',0.001,1,1,512,512)
-# W2 = WeightArray(original_atten_block_weight_array_encoder_2,'array',0.001,1,1,512,512)
-# W3 = WeightArray(original_atten_block_weight_array_encoder_3,'array',0.001,1,1,512,512)
-# W4 = WeightArray(original_atten_block_weight_array_encoder_4,'array',0.001,1,1,512,512)
-# W5 = WeightArray(original_atten_block_weight_array_encoder_5,'array',0.001,1,1,512,512)
+df = pd.DataFrame(results)
 
+# Save the results to a CSV file named 'baseline.csv'
+df.to_csv('baseline.csv', index=False)
 
-# mse = []
-# bleu_scores = []
-# compression_ratio = []
-
-# for i in range(150):  #150
-#         atten_block_weight_array_0 = W0.iterative_approximation(1)
-#         atten_block_weight_array_1 = W1.iterative_approximation(1)
-#         atten_block_weight_array_2 = W2.iterative_approximation(1)
-#         atten_block_weight_array_3 = W3.iterative_approximation(1)
-#         atten_block_weight_array_4 = W4.iterative_approximation(1)
-#         atten_block_weight_array_5 = W5.iterative_approximation(1)
-
-# from tqdm import tqdm
-
-# with tqdm(total=18, desc='Processing', unit='iteration') as pbar:
-#     for i in range(18): #18
-#         for i in range(5):  #5
-#             atten_block_weight_array_0 = W0.iterative_approximation(1)
-#             atten_block_weight_array_1 = W1.iterative_approximation(1)
-#             atten_block_weight_array_2 = W2.iterative_approximation(1)
-#             atten_block_weight_array_3 = W3.iterative_approximation(1)
-#             atten_block_weight_array_4 = W4.iterative_approximation(1)
-#             atten_block_weight_array_5 = W5.iterative_approximation(1)
-
-#         W0_MSE = (W0.average_mse_array()+W1.average_mse_array()+W2.average_mse_array()+W3.average_mse_array()+W4.average_mse_array()+W5.average_mse_array())/6
-#         mse.append(W0_MSE)
-#         compression_ratio.append(W0.compression_ratio())
-
-#         set_layer_weight(model.model.encoder.layers[0],atten_block_weight_array_0)
-#         set_layer_weight(model.model.encoder.layers[1],atten_block_weight_array_1)
-#         set_layer_weight(model.model.encoder.layers[2],atten_block_weight_array_2)
-#         set_layer_weight(model.model.encoder.layers[3],atten_block_weight_array_3)
-#         set_layer_weight(model.model.encoder.layers[4],atten_block_weight_array_4)
-#         set_layer_weight(model.model.encoder.layers[5],atten_block_weight_array_5)
-
-#         bleu = compute_bleu_score(device, model, tokenizer,source_texts,target_texts)
-#         bleu_scores.append(bleu)
-
-#         pbar.update(1)
+print("Results saved to 'baseline.csv'")
 
 
-# print(bleu_scores)
 
