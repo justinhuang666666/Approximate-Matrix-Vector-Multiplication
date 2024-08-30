802983b655aab5e11569839e713754c1579b068c
Justin Huang <yinting_justin_huang@outlook.com>
2024-08-30 13:12:01 +0100

    add gpu support

--- iterative_approximation/iterative_approximation_gpu.py
+++ iterative_approximation/iterative_approximation_gpu.py
@@ -210,4 +210,4 @@ W32.init_precision(32)
 for i in range(10):
     for j in range(10):
         WW_32 = W32.iterative_approximation(1)
-        print(W32.average_mse_array()) 
+    print(W32.average_mse_array()) 
