158cd84798b07970a2ff1f6d1f6fefc896b7e9a8
Justin Huang <yinting_justin_huang@outlook.com>
2024-09-12 22:00:48 +0100

    update quant code

--- server/opus-mt-en-de-quant.py
+++ server/opus-mt-en-de-quant.py
@@ -96,7 +96,7 @@ args_int32.quant_scheme = {
 # Create the quantization scheme using the from_args method
 quant_scheme_int32 = QuantScheme.from_args(args_int32)
 
-quant_scheme_int32 = replace_with_quantized(model, quant_scheme_int32, attention_layer_types)
+quant_scheme_int32 = replace_with_quantized(model, quant_scheme_int32)
 
 bleu_int32 = compute_bleu_score(device, quant_scheme_int32, tokenizer, source_texts, target_texts)
 print("INT32 BLEU Score")
@@ -123,7 +123,7 @@ quant_scheme_int16 = QuantScheme.from_args(args_int16)
 
 
 # INT16_model = replace_with_quantized(model, quant_scheme_int16, attention_layer_types)
-INT16_model = replace_with_quantized(model, quant_scheme_int16, attention_layer_types)
+INT16_model = replace_with_quantized(model, quant_scheme_int16)
 
 bleu_int16 = compute_bleu_score(device, INT16_model, tokenizer, source_texts, target_texts)
 print("INT16 BLEU Score")
