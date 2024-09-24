eaa976e03cc341174e6fa4124f64717b37a18b26
Justin Huang <yinting_justin_huang@outlook.com>
2024-09-24 23:06:29 +0100

    approximation quant

--- iterative_approximation/iterative_approximation_quant.py
+++ iterative_approximation/iterative_approximation_quant.py
@@ -326,7 +326,7 @@ quant_method_int = QuantScheme.from_args(args_int)
 W32 = WeightArray(W,'array',0.001,1,1,512,512,quant_method_int)
 WW_32 = W32.iterative_approximation(1)
 
-for i in range(10):
-    for j in range(10):
-        WW_32 = W32.iterative_approximation(1)
-    print(W32.average_mse_array()) 
+# for i in range(10):
+#     for j in range(10):
+#         WW_32 = W32.iterative_approximation(1)
+#     print(W32.average_mse_array()) 
