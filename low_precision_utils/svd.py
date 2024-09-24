25f0a439c59861aff84de91f97c9e09e94f7957f
Justin Huang <yinting_justin_huang@outlook.com>
2024-09-24 22:44:15 +0100

    approximation quant

--- low_precision_utils/svd.py
+++ low_precision_utils/svd.py
@@ -24,7 +24,7 @@ def quant_svd(u, s, v, quant_scheme: "quant.QuantScheme" = None):
 
     qus = qs.mv(qu) #.mv(qv.t()).to(u_type)
     print('qus')
-    qus.view(*u_shape[-1], -1)
+    qus.view(u_shape[-1], u_shape[-1])
     print(qus)
     qreconstructed = 0 # q.mm(qv.t()).to(u_type)
     # print('reconstructed')
