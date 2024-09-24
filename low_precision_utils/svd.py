7e7cf5092d471a7f752cd4dab5ee7bf8fa030fe1
Justin Huang <yinting_justin_huang@outlook.com>
2024-09-24 22:56:09 +0100

    approximation quant

--- low_precision_utils/svd.py
+++ low_precision_utils/svd.py
@@ -14,22 +14,15 @@ def quant_svd(u, s, v, quant_scheme: "quant.QuantScheme" = None):
 
     qu = quant_scheme.weight.quant(u)    # Quantize U (left singular vector)
     qv = quant_scheme.weight.quant(v)    # Quantize V (right singular vector)
-    
     # Adjust the dimensions of S to match the dimensions of qu and qv
     S = torch.diag(torch.full((qu.size(0),), s)).to(device) 
     qs = quant_scheme.weight.quant(S).to(device)
-    u_shape = u.shape
-    print('u shape:',u_shape[-1])
-    print('qs shape',qs.shape)
-    
 
     qus = qs.mv(qu) #.mv(qv.t()).to(u_type)
-    print('qus')
-    print(qus)
-    qreconstructed = qus.mv(qv.t()).to(u_type)
+    qreconstructed = qus @ qv.t()
     # print('reconstructed')
     # print(qreconstructed)
-    return qreconstructed
+    return qreconstructed.to(u_type)
 
 # import argparse
 # import warnings
