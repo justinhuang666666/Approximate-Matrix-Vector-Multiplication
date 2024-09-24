30e636daaaf8abd587142bb21c3fda979f647ea8
Justin Huang <yinting_justin_huang@outlook.com>
2024-09-24 23:21:16 +0100

    approximation quant

--- iterative_approximation/iterative_approximation_quant.py
+++ iterative_approximation/iterative_approximation_quant.py
@@ -303,8 +303,8 @@ random_matrix3 = np.random.rand(512, 512)
 
 W = [random_matrix1,random_matrix2,random_matrix3]
 
-wl = 16
-frac = 8
+wl = 32
+frac = 18
 symmetric = True
 round_mode = 'nearest'
 
