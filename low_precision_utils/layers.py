eaf555dc5b1f503befa24abfe7b434c26aca7ee7
Justin Huang <yinting_justin_huang@outlook.com>
2024-10-08 23:37:03 +0100

    update quant svd

--- low_precision_utils/layers.py
+++ low_precision_utils/layers.py
@@ -37,9 +37,6 @@ class QuantLinearSVD(nn.Linear):
         self.U = nn.Parameter(torch.empty(out_features, rank, device=device, dtype=dtype))
         self.V = nn.Parameter(torch.empty(rank, in_features, device=device, dtype=dtype))
 
-        print(self.U)
-        print(self.V)
-
         # Initialize weights and quantization scheme
         self.quant_scheme = quant_scheme
 
@@ -53,6 +50,9 @@ class QuantLinearSVD(nn.Linear):
         U, V = compute_uv(module.weight.data, rank)
         l.U.data.copy_(U)
         l.V.data.copy_(V)
+
+        print(l.U)
+        print(l.V)
         if module.bias is not None:
             l.bias.data.copy_(module.bias.data)
         return l
