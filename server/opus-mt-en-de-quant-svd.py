0a187f02954ab6d8fb8b9bd571a969ce1cf01da0
Justin Huang <yinting_justin_huang@outlook.com>
2024-10-09 18:04:25 +0100

    update quant svd

--- server/opus-mt-en-de-quant-svd.py
+++ server/opus-mt-en-de-quant-svd.py
@@ -104,12 +104,12 @@ print(layer)
 
 # print(bleu_int)
 
-# quant_svd_model = replace_with_quantized_svd(model, 312, quant_scheme_int, filter)
+quant_svd_model = replace_with_quantized_svd(model, 312, quant_scheme_int, filter)
 
-# # Compute BLEU score
-# bleu_int = compute_bleu_score(device, quant_svd_model, tokenizer, source_texts, target_texts)
+# Compute BLEU score
+bleu_int = compute_bleu_score(device, quant_svd_model, tokenizer, source_texts, target_texts)
 
-# print(bleu_int)
+print(bleu_int)
 
 # quant_svd_model = replace_with_quantized_svd(model, 112, quant_scheme_int, filter)
 
