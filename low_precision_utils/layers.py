fa84dffe1f06ce54a75bed20d4d1786502eb455a
Justin Huang <yinting_justin_huang@outlook.com>
2024-10-30 14:45:59 +0000

    iterative and normal comparison code

--- low_precision_utils/layers.py
+++ low_precision_utils/layers.py
@@ -77,6 +77,7 @@ class QuantLinearSVD(nn.Linear):
             # Perform matrix multiplication
             vx = torch.matmul(qinput,qv[i,:].T)  # qv[i].T to match dimensions for multiplication
             print(vx.shape)
+            print(qu[:,i].shape)
             qvx = self.quant_scheme.weight.quant(vx)
             output= torch.matmul(qvx, qu[:,i]) # Multiplying by qu without transposing
             qoutput += self.quant_scheme.weight.quant(output)
