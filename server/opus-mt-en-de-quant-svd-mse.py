ef68aed85ad6505cfd265a35b10c2da35c7ecc9e
Justin Huang <yinting_justin_huang@outlook.com>
2024-11-12 10:47:52 +0000

    check mse code update

--- server/opus-mt-en-de-quant-svd-mse.py
+++ server/opus-mt-en-de-quant-svd-mse.py
@@ -230,6 +230,7 @@ for rank in rank_samples:
     approximated_weight_array2 = [u_array2[i] @ v_array2[i] for i in range(len(weight_array))]
     mse1 = mean_square_error_array1(u_array1, u_array2)
     mse2 = mean_square_error_array1(v_array1, v_array2)
+    mse3 = mean_square_error_array1(approximated_weight_array1, approximated_weight_array2)
 
     # mse1_list.append(mse1)
     # mse2_list.append(mse2)
@@ -243,12 +244,15 @@ for rank in rank_samples:
         mse_check_1 = (u_array1[j] - u_array2[j]).pow(2).mean()
         # Compute MSE between approximated_weight_array1 and approximated_weight_array2
         mse_check_2 = (v_array1[j] - v_array2[j]).pow(2).mean()
+        mse_check_3 = (approximated_weight_array1[j] - approximated_weight_array2[j]).pow(2).mean()
 
         print(f"MSE Check 1: {mse_check_1:.20f}")
         print(f"MSE Check 2: {mse_check_2:.20f}")
+        print(f"MSE Check 3: {mse_check_3:.20f}")
 
     print(f"Layer {layer_idx + 1} - MSE 1: {mse1:.20f}")
     print(f"Layer {layer_idx + 1} - MSE 2: {mse2:.20f}")
+    print(f"Layer {layer_idx + 1} - MSE 3: {mse3:.20f}")
     # print(f"Layer {layer_idx + 1} - Diff MSE: {mse1-mse2:.20f}")
 
     # Calculate compression ratio (keeping it constant here as it depends on wl)
