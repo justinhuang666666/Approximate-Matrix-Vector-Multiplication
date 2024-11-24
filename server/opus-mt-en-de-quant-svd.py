fed9b46927aea63f78e1704fc7f058fcd70473b6
Justin Huang <yinting_justin_huang@outlook.com>
2024-11-24 23:23:57 +0000

    different scaling based quant methods

--- server/opus-mt-en-de-quant-svd.py
+++ server/opus-mt-en-de-quant-svd.py
@@ -54,9 +54,9 @@ source_texts = data['source_texts']
 target_texts = data['target_texts']
 
 # Compute BLEU score
-# baseline_bleu = compute_bleu_score(device, model, tokenizer, source_texts, target_texts)
-# print("Baseline BLEU Score")
-# print(baseline_bleu) 
+baseline_bleu = compute_bleu_score(device, model, tokenizer, source_texts, target_texts)
+print("Baseline BLEU Score")
+print(baseline_bleu) 
 
 # Quantisation
 filter = type(model.model.encoder.layers[0])
@@ -65,8 +65,8 @@ filter = type(model.model.encoder.layers[0])
 args_int = argparse.Namespace()
 
 # Define possible values for wl, fl, symmetric, and round_mode
-word_lengths = [8,16]#[4, 6, 8, 16]
-rank_samples_array = [[3,5],[3,5]]# [[250,300,350,400,450],[170,210,250,290,330],[125,155,185,215,245],[65,80,95,110,125]]
+word_lengths = [4, 6, 8, 16]
+rank_samples_array = [[250,300,350,400,450],[170,210,250,290,330],[125,155,185,215,245],[65,80,95,110,125]]
 
 symmetric = True
 round_mode = "nearest"
