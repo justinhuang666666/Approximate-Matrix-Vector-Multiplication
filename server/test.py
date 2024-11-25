20db3264bc97bd9a4ad1cdc9c7b6691fefc628ea
Justin Huang <yinting_justin_huang@outlook.com>
2024-11-25 14:28:00 +0000

    test quant

---
+++ server/test.py
@@ -0,0 +1,68 @@
+import torch
+from transformers import MarianMTModel, MarianTokenizer
+import json
+from utils import *
+import os
+import sys
+import pandas as pd
+
+from torch import nn
+
+import warnings
+
+import argparse
+import itertools
+import csv
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+# Add utility directories dynamically
+sys.path.append(os.path.join(parent_dir, 'low_precision_utils'))
+from quant import *
+from quant_svd import *
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+# Add utility directories dynamically
+sys.path.append(os.path.join(parent_dir, 'iterative_approximation'))
+from iterative_approximation_gpu import *
+
+# Load the tokenizer and model
+model_name = "Helsinki-NLP/opus-mt-en-de"
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+model.eval()
+
+# Check if GPU is available and move model to GPU if possible
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+model.to(device)
+
+# Load the JSON file
+with open('translations.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Extract the source and target texts
+source_texts = data['source_texts']
+target_texts = data['target_texts']
+
+# Compute BLEU score
+baseline_bleu = compute_bleu_score(device, model, tokenizer, source_texts, target_texts)
+print("Baseline BLEU Score")
+print(baseline_bleu) 
+
+weight = model.model.encoder.layers[0].self_attn.k_proj.weight
+print("Original Weight Matrix: ",weight[0:5,0:5])
+quant,dequant,scale = quantisation_wrapper(weight,4)
+
+print("Quant Weight Matrix: ",quant[0:5,0:5])
+print("Dequant Weight Matrix: ",dequant[0:5,0:5])
+print("Scale: ",scale)
