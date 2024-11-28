02fa9e36af906b489c5e971b5a1e464906e7a67e
Justin Huang <yinting_justin_huang@outlook.com>
2024-11-28 23:33:45 +0000

    update quant svd code

--- server/opus-mt-en-de-quant-svd.py
+++ server/opus-mt-en-de-quant-svd.py
@@ -65,8 +65,8 @@ filter = type(model.model.encoder.layers[0])
 args_int = argparse.Namespace()
 
 # Define possible values for wl, fl, symmetric, and round_mode
-word_lengths = [8,16] #[2, 4, 6, 8, 16]
-rank_samples_array = [[64,128],[32,40]] #[[256,320,384,448,512],[128,160,192,224,256],[85,105,125,145,165],[64,80,96,112,128],[32,40,48,56,64]]
+word_lengths = [2, 4, 6, 8, 16]
+rank_samples_array = [[256,320,384,448,512],[128,160,192,224,256],[85,105,125,145,165],[64,80,96,112,128],[32,40,48,56,64]]
 
 symmetric = True
 round_mode = "nearest"
