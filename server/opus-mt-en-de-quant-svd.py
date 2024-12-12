22141a9f5c458b7c555174d328384dbe7117ce27
Justin Huang <yinting_justin_huang@outlook.com>
2024-12-12 23:42:08 +0000

    quant fr en baseline

--- server/opus-mt-en-de-quant-svd.py
+++ server/opus-mt-en-de-quant-svd.py
@@ -68,7 +68,7 @@ filter = type(model.model.encoder.layers[0])
 args_int = argparse.Namespace()
 
 # Define possible values for wl, fl, symmetric, and round_mode
-word_lengths = [2, 3, 4, 6, 8, 16]
+word_lengths = [3, 4, 6, 8]
 rank_samples = [64,80,96,112,128,144,160,176,192,208,224,240,256,272,288,304,320,336,352,368,384,400,416,432,448,464,480,496,512]
 
 symmetric = True
@@ -176,7 +176,7 @@ filter = type(model.model.encoder.layers[0])
 args_int = argparse.Namespace()
 
 # Define possible values for wl, fl, symmetric, and round_mode
-word_lengths = [2, 3, 4, 6, 8, 16]
+word_lengths = [3, 4, 6, 8]
 rank_samples = [64,80,96,112,128,144,160,176,192,208,224,240,256,272,288,304,320,336,352,368,384,400,416,432,448,464,480,496,512]
 
 symmetric = True
