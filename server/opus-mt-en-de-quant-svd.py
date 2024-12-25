844f2fdb47fa4eb1cddf4f3f13f654718fa48b91
Justin Huang <yinting_justin_huang@outlook.com>
2024-12-25 21:31:58 +0100

    update svd

--- server/opus-mt-en-de-quant-svd.py
+++ server/opus-mt-en-de-quant-svd.py
@@ -80,8 +80,8 @@ args = parser.parse_args()
 weight_wl = args.weight_wl
 
 results_list = []
-quant_svd_model = replace_with_quantized_svd(model, 10, weight_wl, "range_based", 16, "range_based", filter)
-quant_iterative_svd_model = replace_with_quantized_iterative_svd(model, 10, weight_wl, "range_based", 16, "range_based", filter)
+quant_svd_model = replace_with_quantized_svd_wrapper(model, 10, weight_wl, "range_based", 16, "range_based", filter)
+quant_iterative_svd_model = replace_with_quantized_svd_wrapper(model, 10, weight_wl, "range_based", 16, "range_based", filter)
 
 for act_wl in act_word_lengths:
     for rank in rank_samples:
