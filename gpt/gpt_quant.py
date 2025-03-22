f92197e4ff78c3fd40ecb32eef297bcb4fd6397d
Justin Huang <yinting_justin_huang@outlook.com>
2025-03-22 21:36:07 +0000

    llama

--- gpt/gpt_quant.py
+++ gpt/gpt_quant.py
@@ -11,6 +11,8 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.append(os.path.join(parent_dir, 'low_precision_utils'))
 from quant import *
 
+import pandas as pd
+
 # Load tokenizer and model
 model_name = "EleutherAI/gpt-neo-1.3B"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -27,8 +29,8 @@ print('baseline perplexity:', ppl)
 filter = type(model.transformer.h[0].attn)
 
 # Define possible values for wl, fl, symmetric, and round_mode
-weight_word_lengths = [2,8] #[2, 4, 6, 8, 10, 12, 14, 16]
-act_word_lengths = [4,8] #[4, 6, 8, 16]
+weight_word_lengths = [2, 4, 6, 8, 10, 12, 14, 16]
+act_word_lengths = [4, 6, 8, 16]
 
 results_list = []
 
@@ -40,17 +42,23 @@ for act_wl in act_word_lengths:
             int_model = replace_with_quantized(model, weight_wl, "range_based", act_wl, "range_based", filter)
 
             # Compute BLEU score
-            perplexity = ppl = compute_ppl(int_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+            wiki_perplexity = compute_ppl(int_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+            ptb_perplexity = compute_ppl(int_model, tokenizer, 'ptb', model_seq_len=2048, batch_size=4, device="cuda")
+            c4_perplexity = compute_ppl(int_model, tokenizer, 'c4', model_seq_len=2048, batch_size=4, device="cuda")
 
             # Print BLEU score
             print(f"GPT-NEO-1.3B INT Wikitext2 Perplexity for weight_wl={weight_wl} act_wl={act_wl}")
-            print("Range-Based: ", perplexity)
+            print("Wikitext2 Perplexity: ", wiki_perplexity)
+            print("PTB Perplexity: ", ptb_perplexity)
+            print("C4 Perplexity: ", c4_perplexity)
 
             # Store the results
             results_list.append({
             "Weight Word Length": weight_wl,
             "Activation Word Length": act_wl,
-            "Perplexity": perplexity,
+            "Wiki Perplexity": wiki_perplexity,
+            "PTB Perplexity": ptb_perplexity,
+            "C4 Perplexity": c4_perplexity,
             "Compression Ratio": 32/weight_wl
             })
 
@@ -58,4 +66,4 @@ for act_wl in act_word_lengths:
 results_df = pd.DataFrame(results_list)
 
 # Save results to a CSV file
-results_df.to_csv('quantization_inout_en_de3.csv', index=False)
+results_df.to_csv('gpt_quantization.csv', index=False)
