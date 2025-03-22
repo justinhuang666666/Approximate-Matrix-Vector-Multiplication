2d01309bfa1df073b864317c0d2ba56714b2b369
Justin Huang <yinting_justin_huang@outlook.com>
2025-03-22 21:20:18 +0000

    llama

---
+++ gpt/gpt_quant.py
@@ -0,0 +1,61 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from evaluater import *
+
+import warnings
+warnings.filterwarnings("ignore")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(os.path.join(parent_dir, 'low_precision_utils'))
+from quant import *
+
+# Load tokenizer and model
+model_name = "EleutherAI/gpt-neo-1.3B"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float32)
+
+# Move model to CUDA if available
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = model.to(device)
+
+ppl = compute_ppl(model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+print('baseline perplexity:', ppl)
+
+# Quantisation
+filter = type(model.transformer.h[0])
+
+# Define possible values for wl, fl, symmetric, and round_mode
+weight_word_lengths = [2,8] #[2, 4, 6, 8, 10, 12, 14, 16]
+act_word_lengths = [4,8] #[4, 6, 8, 16]
+
+results_list = []
+
+for act_wl in act_word_lengths:
+    # Iterate over all combinations of wl, fl, symmetric, and round_mode
+    for weight_wl in weight_word_lengths: 
+
+            # Replace with quantized model
+            int_model = replace_with_quantized(model, weight_wl, "range_based", act_wl, "range_based", filter)
+
+            # Compute BLEU score
+            perplexity = ppl = compute_ppl(int_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+
+            # Print BLEU score
+            print(f"GPT-NEO-1.3B INT Wikitext2 Perplexity for weight_wl={weight_wl} act_wl={act_wl}")
+            print("Range-Based: ", perplexity)
+
+            # Store the results
+            results_list.append({
+            "Weight Word Length": weight_wl,
+            "Activation Word Length": act_wl,
+            "Perplexity": perplexity,
+            "Compression Ratio": 32/weight_wl
+            })
+
+# Convert the list of dictionaries to a DataFrame
+results_df = pd.DataFrame(results_list)
+
+# Save results to a CSV file
+results_df.to_csv('quantization_inout_en_de3.csv', index=False)
