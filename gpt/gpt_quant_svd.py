01311af3801cebb88012fd98aab14a3ae438dabe
Justin Huang <yinting_justin_huang@outlook.com>
2025-03-24 09:40:13 +0000

    update full run for gpt quant svd

--- gpt/gpt_quant_svd.py
+++ gpt/gpt_quant_svd.py
@@ -24,9 +24,9 @@ model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = model.to(device)
 
-# baseline_wiki_perplexity = compute_ppl(model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+baseline_wiki_perplexity = compute_ppl(model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
 # baseline_ptb_perplexity = compute_ppl(model, tokenizer, 'ptb', model_seq_len=2048, batch_size=4, device="cuda")
-# print('Baseline Wikitext2 Perplexity:', baseline_wiki_perplexity)
+print('Baseline Wikitext2 Perplexity:', baseline_wiki_perplexity)
 # print('Baseline PTB Perplexity:', baseline_ptb_perplexity)
 
 # Quantisation
@@ -45,54 +45,54 @@ print(rank_samples)
 
 # weight_wl = args.weight_wl
 
-# weight_wl = 8
-# act_word_lengths = [8, 16]
+weight_wl = 8
+act_word_lengths = [8, 16]
 
-# results_list = []
-# print("Start decomposing SVD model...")
-# start_time = time.time()
-# quant_svd_model = replace_with_quantized_svd_wrapper(model, 1152, weight_wl, "range_based", 16, "range_based", filter)
-# end_time = time.time()
-# elapsed_time = end_time - start_time
-# print(f"SVD decomposition time: {elapsed_time:.4f} seconds")
+results_list = []
+print("Start decomposing SVD model...")
+start_time = time.time()
+quant_svd_model = replace_with_quantized_svd_wrapper(model, 1160, weight_wl, "range_based", 16, "range_based", filter)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"SVD decomposition time: {elapsed_time:.4f} seconds")
 
 
-# print("Start decomposing iterative SVD model...")
-# start_time = time.time()
-# quant_iterative_svd_model = replace_with_quantized_iterative_svd(model, 1152, weight_wl, "range_based", 16, "range_based", filter)
-# end_time = time.time()
-# elapsed_time = end_time - start_time
-# print(f"Iterative SVD decomposition time: {elapsed_time:.4f} seconds")
+print("Start decomposing iterative SVD model...")
+start_time = time.time()
+quant_iterative_svd_model = replace_with_quantized_iterative_svd(model, 1160, weight_wl, "range_based", 16, "range_based", filter)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Iterative SVD decomposition time: {elapsed_time:.4f} seconds")
 
-# for act_wl in act_word_lengths:
+for act_wl in act_word_lengths:
 
-#     for rank in rank_samples:
-#         print(f"GPT-NEO-1B INT Model for weight_wl={weight_wl}, act_wl={act_wl}, rank={rank}")
-#         # Compute BLEU score
-#         quant_svd_model = change_rank(quant_svd_model, rank, act_wl, filter)
-#         wiki_perplexity1 = compute_ppl(quant_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
-#         print("Quant SVD Perplexity: ", wiki_perplexity1)
+    for rank in rank_samples:
+        print(f"GPT-NEO-1B INT Model for weight_wl={weight_wl}, act_wl={act_wl}, rank={rank}")
+        # Compute BLEU score
+        quant_svd_model = change_rank(quant_svd_model, rank, act_wl, filter)
+        wiki_perplexity1 = compute_ppl(quant_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+        print("Quant SVD Perplexity: ", wiki_perplexity1)
         
-#         quant_iterative_svd_model = change_rank(quant_iterative_svd_model, rank, act_wl, filter)
-#         # Compute BLEU score
-#         wiki_perplexity2 = compute_ppl(quant_iterative_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
-#         print("Iterative Quant SVD Perplexity",wiki_perplexity2)
+        quant_iterative_svd_model = change_rank(quant_iterative_svd_model, rank, act_wl, filter)
+        # Compute BLEU score
+        wiki_perplexity2 = compute_ppl(quant_iterative_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+        print("Iterative Quant SVD Perplexity",wiki_perplexity2)
 
-#         compression_ratio = 2048*2048*3*24*32/(rank*(2048*2)*3*24*weight_wl)
+        compression_ratio = 2048*2048*3*24*32/(rank*(2048*2)*3*24*weight_wl)
 
-#         # Store the results
-#         results_list.append({
-#         "Weight Word Length": weight_wl,
-#         "Activation Word Length": act_wl,
-#         "Rank":rank,
-#         "Quant SVD Perplexity": wiki_perplexity1,
-#         "Iterative Quant SVD Perplexity": wiki_perplexity2,
-#         "Compression Ratio":compression_ratio
-#         })
+        # Store the results
+        results_list.append({
+        "Weight Word Length": weight_wl,
+        "Activation Word Length": act_wl,
+        "Rank":rank,
+        "Quant SVD Perplexity": wiki_perplexity1,
+        "Iterative Quant SVD Perplexity": wiki_perplexity2,
+        "Compression Ratio":compression_ratio
+        })
 
-# # Convert the list of dictionaries to a DataFrame
-# results_df = pd.DataFrame(results_list)
+# Convert the list of dictionaries to a DataFrame
+results_df = pd.DataFrame(results_list)
 
-# # Save results to a CSV file
-# results_df.to_csv(f'gpt_quant_svd_{weight_wl}.csv', index=False)
+# Save results to a CSV file
+results_df.to_csv(f'gpt_quant_svd_{weight_wl}.csv', index=False)
 
