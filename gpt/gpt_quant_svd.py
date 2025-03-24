03ec448d068b6e184a7eb3674566fa48836e7827
Justin Huang <yinting_justin_huang@outlook.com>
2025-03-24 09:26:14 +0000

    update full run for gpt quant svd

--- gpt/gpt_quant_svd.py
+++ gpt/gpt_quant_svd.py
@@ -13,6 +13,8 @@ from quant import *
 
 import pandas as pd
 
+import time
+
 # Load tokenizer and model
 model_name = "EleutherAI/gpt-neo-1.3B"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -32,7 +34,7 @@ filter = type(model.transformer.h[0].attn)
 
 # rank_samples = list(range(256, 2048 + 1, 256))
 
-rank_samples = list(range(32, 128 + 1, 32))
+rank_samples = list(range(256, 2048 + 1, 128))
 
 print(rank_samples)
 
@@ -44,37 +46,49 @@ print(rank_samples)
 # weight_wl = args.weight_wl
 
 weight_wl = 8
-act_wl = 8
+act_word_lengths = [8, 16]
 
 results_list = []
-quant_svd_model = replace_with_quantized_svd_wrapper(model, 128, weight_wl, "range_based", 16, "range_based", filter)
-quant_iterative_svd_model = replace_with_quantized_iterative_svd(model, 128, weight_wl, "range_based", 16, "range_based", filter)
+print("Start decomposing SVD model...")
+start_time = time.time()
+quant_svd_model = replace_with_quantized_svd_wrapper(model, 2048, weight_wl, "range_based", 16, "range_based", filter)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"SVD decomposition time: {elapsed_time:.4f} seconds")
 
-# for act_wl in act_word_lengths:
 
-for rank in rank_samples:
-    print(f"GPT-NEO-1B INT Model for weight_wl={weight_wl}, act_wl={act_wl}, rank={rank}")
-    # Compute BLEU score
-    quant_svd_model = change_rank(quant_svd_model, rank, act_wl, filter)
-    wiki_perplexity1 = compute_ppl(quant_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
-    print("Quant SVD Perplexity: ", wiki_perplexity1)
-    
-    quant_iterative_svd_model = change_rank(quant_iterative_svd_model, rank, act_wl, filter)
-    # Compute BLEU score
-    wiki_perplexity2 = compute_ppl(quant_iterative_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
-    print("Iterative Quant SVD Perplexity",wiki_perplexity2)
+print("Start decomposing iterative SVD model...")
+start_time = time.time()
+quant_iterative_svd_model = replace_with_quantized_iterative_svd(model, 2048, weight_wl, "range_based", 16, "range_based", filter)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Iterative SVD decomposition time: {elapsed_time:.4f} seconds")
 
-    compression_ratio = 2048*2048*3*24*32/(rank*(2048*2)*3*24*weight_wl)
+for act_wl in act_word_lengths:
 
-    # Store the results
-    results_list.append({
-    "Weight Word Length": weight_wl,
-    "Activation Word Length": act_wl,
-    "Rank":rank,
-    "Quant SVD Perplexity": wiki_perplexity1,
-    "Iterative Quant SVD Perplexity": wiki_perplexity2,
-    "Compression Ratio":compression_ratio
-    })
+    for rank in rank_samples:
+        print(f"GPT-NEO-1B INT Model for weight_wl={weight_wl}, act_wl={act_wl}, rank={rank}")
+        # Compute BLEU score
+        quant_svd_model = change_rank(quant_svd_model, rank, act_wl, filter)
+        wiki_perplexity1 = compute_ppl(quant_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+        print("Quant SVD Perplexity: ", wiki_perplexity1)
+        
+        quant_iterative_svd_model = change_rank(quant_iterative_svd_model, rank, act_wl, filter)
+        # Compute BLEU score
+        wiki_perplexity2 = compute_ppl(quant_iterative_svd_model, tokenizer, 'wikitext2', model_seq_len=2048, batch_size=4, device="cuda")
+        print("Iterative Quant SVD Perplexity",wiki_perplexity2)
+
+        compression_ratio = 2048*2048*3*24*32/(rank*(2048*2)*3*24*weight_wl)
+
+        # Store the results
+        results_list.append({
+        "Weight Word Length": weight_wl,
+        "Activation Word Length": act_wl,
+        "Rank":rank,
+        "Quant SVD Perplexity": wiki_perplexity1,
+        "Iterative Quant SVD Perplexity": wiki_perplexity2,
+        "Compression Ratio":compression_ratio
+        })
 
 # Convert the list of dictionaries to a DataFrame
 results_df = pd.DataFrame(results_list)
