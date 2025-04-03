bbd496017fb41b5831489392fe20b2a472a1be77
Justin Huang <yinting_justin_huang@outlook.com>
2025-04-03 08:31:47 +0100

    update full run for gpt quant svd

---
+++ gpt/svd_test.py
@@ -0,0 +1,32 @@
+import torch
+
+def rank1_full_svd(A):
+    # Full SVD decomposition
+    U, S, Vh = torch.linalg.svd(A, full_matrices=False)
+    sigma = S[0]
+    u1 = U[:, 0].reshape(-1, 1) * torch.sqrt(sigma)
+    v1 = Vh[0, :].reshape(1, -1) * torch.sqrt(sigma)
+    A_approx = u1 @ v1
+    return A_approx, u1, v1, sigma
+
+def rank1_lowrank_svd(A):
+    # Approximate rank-1 SVD
+    U, S, V = torch.linalg.svd_lowrank(A, q=1, niter=2)
+    sigma = S[0]
+    u1 = U[:, 0].reshape(-1, 1) * torch.sqrt(sigma)
+    v1 = V[0, :].reshape(1, -1) * torch.sqrt(sigma)
+    A_approx = u1 @ v1
+    return A_approx, u1, v1, sigma
+
+# Generate a random 2048 x 2048 matrix
+torch.manual_seed(0)
+A = torch.randn(2048, 2048, dtype=torch.float64)
+
+# Method 1: Full SVD
+A1, u1, v1, sigma1 = rank1_full_svd(A)
+
+# Method 2: Low-rank SVD
+A2, u2, v2, sigma2 = rank1_lowrank_svd(A)
+
+# Print Frobenius norm difference between methods
+print("Frobenius norm ||A1 - A2||:", torch.norm(A1 - A2).item())
